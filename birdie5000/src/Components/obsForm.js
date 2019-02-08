d2e8d93dcb8b6d1f0755e7a1f3a1bc1a19ad3fba
teepiik <teemu.piik@gmail.com>
2019-02-08 16:10:11 +0200

    Router, obsform, broken build

--- birdie5000/src/Components/obsForm.js
+++ birdie5000/src/Components/obsForm.js
@@ -11,6 +11,30 @@ class ObsForm extends React.Component {
             notes: ''
         }
     }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addobservationr({
+            birdname: this.state.birdname,
+            birdrarity: this.state.birdrarity,
+            date: this.state.date,
+            notes: this.state.notes
+        })
+        this.props.history.push('/')
+    }
+
+
+    render() {
+        return(
+            <div>
+                <h2>Add new observation</h2>
+            </div>
+        )
+    }
 }
 
 export default ObsForm
--- birdie5000/src/Components/observation.js
+++ birdie5000/src/Components/observation.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Observation = ({observation}) => {
     return (
+        <div>
         <li className="Observation"> 
             <h2>{observation.birdname}</h2>
             <p>{observation.birdrarity}</p>
             <p>{observation.notes}</p>
             <p>{observation.date}</p>
         </li>
+        </div>
     )
 }
 
